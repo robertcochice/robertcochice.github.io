f964b69e2f2be45216ad440c2633beb99617412d
Robertcwesson <87876454+Robertcwesson@users.noreply.github.com>
2023-08-30 19:03:38 -0400

    Changes

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/componets/header.css';
+import '../styles/hero.css';
+import '../styles/componets/featured.css';
+import '../styles/componets/work.css';
+// import '../styles/componets/contact.css';
+import '../styles/componets/footer.css';
+// import '../styles/componets/mobile-nav.css';
+import '../styles/componets/about.css';
+import '../styles/utils.css';
+
+ 
